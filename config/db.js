a699af10ef64afe982e41eb61b289772f663c910
Surya86 <surya.tectum@gmail.com>
2025-04-21 11:55:29 +0530

    Some update changes

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: "#supra@25",
     database: process.env.DB_NAME,
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
     charset: 'utf8mb4',
